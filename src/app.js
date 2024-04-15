c174b7fb75a9a3cefc8fcb9787998f6902ac5de3
Gabriel Roveri <groveri.gr@outlook.com>
2024-04-15 20:27:09 -0300

    Add DB configs & db schema

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dbConnection from "./config/dbConnect.js";
+import book from "./models/Book.js";
 
 const conection = await dbConnection();
 
@@ -14,42 +15,14 @@ conection.once("open", () => {
 const app = express();
 //middleware
 app.use(express.json());
-//Test data
-const books_data = [
-    { "id": 1, "title": "Tin" },
-    {"id":2,"title":"Home Ing"},
-    {"id":3,"title":"Tres-Zap"},
-    {"id":4,"title":"Bitchip"},
-    {"id":5,"title":"Alpha"},
-    {"id":6,"title":"Temp"},
-    {"id":7,"title":"Mat Lam Tam"},
-    {"id":8,"title":"Tampflex"},
-    {"id":9,"title":"Sub-Ex"},
-    {"id":10,"title":"Zathin"},
-    {"id":11,"title":"Matsoft"},
-    {"id":12,"title":"Tampflex"},
-    {"id":13,"title":"Toughjoyfax"},
-    {"id":14,"title":"Lotlux"},
-    {"id":15,"title":"Andalax"},
-    {"id":16,"title":"Holdlamis"},
-    {"id":17,"title":"Ventosanzap"},
-    {"id":18,"title":"Alphazap"},
-    {"id":19,"title":"Konklab"},
-    { "id": 20, "title": "Matsoft" }
-]
-
-function bookSearcher(id) {
-    return books_data.findIndex(book => {
-        return book.id === Number(id);
-    })
-};
 
 app.get("/", (req, res) => {
     res.status(200).send("Express test");
 });
 
-app.get("/books", (req, res) => {
-    res.status(200).json(books_data);
+app.get("/books", async (req, res) => {
+    const booksList = await book.find({});
+    res.status(200).json(booksList);
 });
 
 app.get("/books/:id", (req, res) => {
