ea0d2c0a3a34792167a4ea19c0d452693af46bd1
Gabriel Roveri <groveri.gr@outlook.com>
2024-04-23 18:57:25 -0300

    Refactored POST & PUT methods

--- src/app.js
+++ src/app.js
@@ -5,34 +5,15 @@ import routes from "./routes/index.js";
 const conection = await dbConnection();
 
 conection.on("error", (error) => {
-    console.error("Erro de conexão", error);
+    console.error("Error on connection!", error);
 });
 
 conection.once("open", () => {
-    console.log("Conexão bem sucedida");
+    console.log("Connection has been sucessful!");
 });
 
 const app = express();
 routes(app);
-
-app.get("/books/:id", (req, res) => {
-    const index = bookSearcher(req.params.id);
-    res.status(201).json(books_data[index]);
-});
-
-//Changing book title
-app.put("/books/:id", (req, res) => {
-    const index = bookSearcher(req.params.id);
-    books_data[index].title = req.body.title;
-    res.status(200).json(books_data);
-});
-
-//Delete book
-app.delete("/books/:id", (req, res) => {
-    const index = bookSearcher(req.params.id);
-    books_data.splice(index, 1);
-    res.status(200).send("Deleted!");
-});
 
 export default app;
 
